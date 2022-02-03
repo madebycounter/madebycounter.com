6b1824cbd3e44e0365ec71b34b2d10f0f374b88b
William Gardner <william@gardna.net>
2022-02-02 21:09:24 -0800

    Disabled jumbotron resize if scrolling

--- assets/js/jumbotron.js
+++ assets/js/jumbotron.js
@@ -1,9 +1,27 @@
-function jumbotron() {
-    var navHeight = $("#_jumbotron_nav").outerHeight() || 0;
-    var windowHeight = window.innerHeight;
+var _jumbotron_scrolling = false;
 
-    $("#_jumbotron").outerHeight(windowHeight - navHeight);
+function jumbotron() {
+    if (!_jumbotron_scrolling) {
+        var navHeight = $("#_jumbotron_nav").outerHeight() || 0;
+        var windowHeight = window.innerHeight;
+    
+        $("#_jumbotron").outerHeight(windowHeight - navHeight);
+    }
 }
 
+$(window).scroll(
+    $.debounce(250, true, function () {
+        _jumbotron_scrolling = true;
+    })
+);
+
+$(window).scroll(
+    $.debounce(250, function () {
+        _jumbotron_scrolling = false;
+    })
+);
+
+$("#_jumbotron_nav").load(jumbotron);
 $(window).load(jumbotron);
 $(window).resize(jumbotron);
+jumbotron();
