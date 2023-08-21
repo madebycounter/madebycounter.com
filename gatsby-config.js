03b6df54ace25ecc8a309c965444dac05109bb77
William Gardner <william@gardna.net>
2023-08-20 19:01:16 -0700

    Added CONTENTFUL_HOST to env vars

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
             options: {
                 accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
                 spaceId: process.env.CONTENTFUL_SPACE_ID,
+                host: process.env.CONTENTFUL_HOST,
             },
         },
         "gatsby-plugin-image",
