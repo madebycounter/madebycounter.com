da24729d47fc66f6b8b7bcecd077c2b41345106e
William Gardner <william@gardna.net>
2023-08-29 11:00:19 -0700

    Extracted common render types to functions

--- src/global/richTextOptions.tsx
+++ src/global/richTextOptions.tsx
@@ -33,23 +33,35 @@ const BlogHeading2 = styled(Heading2)`
     margin: 1rem 0;
 `;
 
+function renderEmbed(node: Block | Inline, children: ReactNode) {
+    const { url, platform } = node.data.target as SocialMediaEmbed;
+
+    if (platform === "Instagram") return <InstagramEmbed url={url} />;
+    else return <p>Unsupported embed</p>;
+}
+
+function renderMedia(node: Block | Inline, children: ReactNode) {
+    return <Media src={node.data.target} />;
+}
+
+function renderCode(text: string) {
+    var language;
+    var split = text.split("\n");
+
+    if (split[0].startsWith("#")) language = split[0].substring(1);
+    text = split.slice(1).join("\n");
+
+    return (
+        <StyledCodeBlock>
+            <Highlight className={language}>{text}</Highlight>
+        </StyledCodeBlock>
+    );
+}
+
 export const blogPostOptions: any = {
     renderNode: {
-        [BLOCKS.EMBEDDED_ASSET]: (
-            node: Block | Inline,
-            children: ReactNode,
-        ) => {
-            return <Media src={node.data.target} />;
-        },
-        [BLOCKS.EMBEDDED_ENTRY]: (
-            node: Block | Inline,
-            children: ReactNode,
-        ) => {
-            const { url, platform } = node.data.target as SocialMediaEmbed;
-
-            if (platform === "Instagram") return <InstagramEmbed url={url} />;
-            else return <p>Unsupported embed</p>;
-        },
+        [BLOCKS.EMBEDDED_ASSET]: renderMedia,
+        [BLOCKS.EMBEDDED_ENTRY]: renderEmbed,
         [BLOCKS.HEADING_1]: (node: Block | Inline, children: ReactNode) => {
             return <BlogHeading1>{children}</BlogHeading1>;
         },
@@ -70,19 +82,7 @@ export const blogPostOptions: any = {
         },
     },
     renderMark: {
-        [MARKS.CODE]: (text: string) => {
-            var language;
-            var split = text.split("\n");
-
-            if (split[0].startsWith("#")) language = split[0].substring(1);
-            text = split.slice(1).join("\n");
-
-            return (
-                <StyledCodeBlock>
-                    <Highlight className={language}>{text}</Highlight>
-                </StyledCodeBlock>
-            );
-        },
+        [MARKS.CODE]: renderCode,
     },
 };
 
