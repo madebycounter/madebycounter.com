6059da97e9d3eab7150bf26a7d3e1831c0a28478
William Gardner <william@madebycounter.com>
2023-10-25 15:08:26 -0700

    Fixed offset bug caused by driven div being smaller than window height

--- src/components/Parallax.tsx
+++ src/components/Parallax.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { forwardRef, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
 const ParallaxWrapper = styled.div`
@@ -9,26 +9,61 @@ const ParallaxWrapper = styled.div`
 type ParallaxProps = {
     children: React.ReactNode;
     className?: string;
+    driverRef: React.RefObject<HTMLDivElement>;
 };
 
-export default function Parallax({ children, className }: ParallaxProps) {
-    return <ParallaxWrapper className={className}>{children}</ParallaxWrapper>;
-}
-
-type ParallaxScrollerProps = ParallaxProps & {
-    parallaxRef: React.RefObject<HTMLDivElement>;
-};
-
-export function ParallaxScroller({
+export default function Parallax({
     children,
     className,
-}: ParallaxScrollerProps) {
-    const [offset, setOffset] = useState(0);
+    driverRef,
+}: ParallaxProps) {
+    const drivenRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
-        const onScroll = () => setOffset(window.scrollY);
+        const onScroll = () => {
+            if (!driverRef.current) return;
+            if (!drivenRef.current) return;
+
+            var progress =
+                (window.scrollY - driverRef.current.offsetTop) /
+                (driverRef.current.offsetHeight - window.innerHeight);
+
+            var offset =
+                (drivenRef.current.offsetHeight - window.innerHeight) *
+                progress;
+
+            if (drivenRef.current.offsetHeight < window.innerHeight) {
+                offset = 0;
+            }
+
+            drivenRef.current.style.top = `-${offset}px`;
+        };
         window.addEventListener("scroll", onScroll);
 
         return () => window.removeEventListener("scroll", onScroll);
     }, []);
+
+    return (
+        <ParallaxWrapper className={className} ref={drivenRef}>
+            {children}
+        </ParallaxWrapper>
+    );
 }
+
+const ParallaxDriverWrapper = styled.div``;
+
+type ParallaxDriverProps = {
+    children: React.ReactNode;
+    className?: string;
+};
+
+export const ParallaxDriver = forwardRef(function (
+    { children, className }: ParallaxDriverProps,
+    ref: React.Ref<HTMLDivElement>,
+) {
+    return (
+        <ParallaxDriverWrapper className={className} ref={ref}>
+            {children}
+        </ParallaxDriverWrapper>
+    );
+});
