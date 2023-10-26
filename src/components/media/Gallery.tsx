867e83951f962965861d6a22dd060a181b0d80f2
William Gardner <william@madebycounter.com>
2023-10-25 19:24:40 -0700

    Implemented intelligent image placement algorithm

--- src/components/media/Gallery.tsx
+++ src/components/media/Gallery.tsx
@@ -35,49 +35,13 @@ const StyledGallery = styled.div<{ $gap: number }>`
     }
 `;
 
-type FixedGalleryProps = {
-    images: Asset[];
-    columns: number;
-    gap?: number;
-    onClick?: (src: string) => void;
-};
-
-const FixedGallery = forwardRef(
-    (
-        { images, columns, gap = 8, onClick }: FixedGalleryProps,
-        ref: React.Ref<HTMLDivElement>,
-    ) => {
-        var cols: Asset[][] = [];
-        for (let i = 0; i < columns; i++) cols.push([]);
-
-        if (images) {
-            for (let i = 0; i < images.length; i++)
-                cols[i % columns].push(images[i]);
-        }
-
-        return (
-            <StyledGallery $gap={gap} ref={ref}>
-                {cols.map((col, i) => (
-                    <div key={i}>
-                        {col.map((img, j) => (
-                            <div key={j}>
-                                <Media src={img} onClick={onClick} />
-                            </div>
-                        ))}
-                    </div>
-                ))}
-            </StyledGallery>
-        );
-    },
-);
-
 type GalleryProps = {
     images: Asset[];
     gap?: number;
     onClick?: (src: string) => void;
 };
 
-export default function Gallery({ images, gap, onClick }: GalleryProps) {
+export default function Gallery({ images, gap = 8, onClick }: GalleryProps) {
     const [ref, size] = useSize<HTMLDivElement>();
     const [columns, setColums] = useState(2);
 
@@ -87,13 +51,129 @@ export default function Gallery({ images, gap, onClick }: GalleryProps) {
         else setColums(3);
     }, [size]);
 
+    var cols = arrangeImages(images, columns, size.width, gap);
+
     return (
-        <FixedGallery
-            columns={columns}
-            images={images}
-            gap={gap}
-            onClick={onClick}
-            ref={ref}
-        />
+        <StyledGallery $gap={gap} ref={ref}>
+            {cols.map((col, i) => (
+                <div key={i}>
+                    {col.images.map((imgData, j) => (
+                        <div key={j}>
+                            <Media
+                                src={imgData.asset}
+                                aspectRatio={
+                                    imgData.normalDimensions.width /
+                                    imgData.normalDimensions.height
+                                }
+                                onClick={onClick}
+                            />
+                        </div>
+                    ))}
+                </div>
+            ))}
+        </StyledGallery>
     );
+}
+
+type ColumnImage = {
+    asset: Asset;
+    normalDimensions: { width: number; height: number };
+};
+
+type Column = {
+    images: ColumnImage[];
+    height: number;
+};
+
+function arrangeImages(
+    images: Asset[],
+    columns: number,
+    width: number,
+    gap: number,
+) {
+    // Find column width
+    var columnWidth = (width - gap * (columns - 1)) / columns;
+
+    // Convert assets to ColumnImage
+    var columnImages: ColumnImage[] = images.map((img) => {
+        var aspectRatio = img.dimensions.width / img.dimensions.height;
+
+        return {
+            asset: img,
+            normalDimensions: {
+                width: columnWidth,
+                height: columnWidth / aspectRatio,
+            },
+        };
+    });
+
+    // Sort images by normalized height
+    columnImages.sort(
+        (a, b) => a.normalDimensions.height - b.normalDimensions.height,
+    );
+
+    // Create array of columns
+    var cols: Column[] = [];
+    for (let i = 0; i < columns; i++) cols.push({ images: [], height: 0 });
+
+    // Add images to shortest column until all images are added
+    for (let i = 0; i < columnImages.length; i++) {
+        var img = columnImages[i];
+        var shortestColumn = cols.reduce((prev, curr) =>
+            prev.height < curr.height ? prev : curr,
+        );
+
+        shortestColumn.images.push(img);
+        shortestColumn.height += img.normalDimensions.height;
+    }
+
+    // Adjust column heights by gap according to image count
+    for (let i = 0; i < columns; i++) {
+        var col = cols[i];
+        col.height += gap * (col.images.length - 1);
+    }
+
+    // Find average column height
+    var averageHeight =
+        cols.reduce((prev, curr) => prev + curr.height, 0) / columns;
+
+    // Adjust image height to match average column height
+    for (let i = 0; i < columns; i++) {
+        var col = cols[i];
+        var diff = (averageHeight - col.height) / col.images.length;
+
+        for (let j = 0; j < col.images.length; j++) {
+            col.images[j].normalDimensions.height += diff;
+        }
+    }
+
+    // Shuffle images deterministically
+    for (let i = 0; i < columns; i++) {
+        cols[i].images = shuffle(cols[i].images, 1);
+    }
+
+    return cols;
+}
+
+// https://github.com/yixizhang/seed-shuffle/blob/master/index.js
+function shuffle(array: any[], seed: number) {
+    let currentIndex = array.length,
+        temporaryValue,
+        randomIndex;
+    seed = seed || 1;
+    let random = function () {
+        var x = Math.sin(seed++) * 10000;
+        return x - Math.floor(x);
+    };
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(random() * currentIndex);
+        currentIndex -= 1;
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
 }
