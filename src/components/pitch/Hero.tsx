18358a22262ee34b6e5633f4f3f4d7d64c529505
William Gardner <william@madebycounter.com>
2023-12-04 22:17:01 -0800

    Refactored pitch page hero

---
+++ src/components/pitch/Hero.tsx
@@ -0,0 +1,87 @@
+import { renderRichText } from "gatsby-source-contentful/rich-text";
+import React from "react";
+import styled from "styled-components";
+
+import { portfolioOptions } from "../../global/richTextOptions";
+
+import { packRichText } from "../../types/RichText";
+import Service from "../../types/Service";
+import Button from "../Button";
+import { Heading1, Heading2, Paragraph } from "../Typography";
+import Slideshow from "../media/Slideshow";
+import YouTube from "../media/YouTube";
+
+const HeroHeading = styled(Heading1)`
+    font-size: 6vw;
+
+    @media (max-width: 1200px) {
+        font-size: 4rem;
+    }
+`;
+
+const HeroDetails = styled.div`
+    flex: 1;
+
+    ${Paragraph} {
+        max-width: 70%;
+    }
+`;
+
+const HeroSlideshow = styled.div`
+    flex: 1;
+`;
+
+const HeroButton = styled(Button)`
+    font-size: 2.5rem;
+
+    ${Heading2} {
+        padding: 0.1em;
+    }
+`;
+
+const HeroWrapper = styled.div`
+    display: flex;
+    gap: 1rem;
+
+    @media (max-width: 850px) {
+        flex-direction: column-reverse;
+    }
+`;
+
+type HeroProps = {
+    service: Service;
+};
+
+export default function Hero({ service }: HeroProps) {
+    return (
+        <HeroWrapper>
+            <HeroDetails>
+                <HeroHeading>{service.pitchTitle}</HeroHeading>
+
+                <Paragraph>
+                    {renderRichText(
+                        packRichText(service.description),
+                        portfolioOptions,
+                    )}
+                </Paragraph>
+
+                <HeroButton to="#" type="normal" direction="right">
+                    {service.callToAction}
+                </HeroButton>
+            </HeroDetails>
+
+            <HeroSlideshow>
+                {!service.youTube && (
+                    <Slideshow
+                        src={service.slideshow.items}
+                        aspectRatio={16 / 9}
+                    />
+                )}
+
+                {service.youTube && (
+                    <YouTube url={service.youTube} aspectRatio={16 / 9} />
+                )}
+            </HeroSlideshow>
+        </HeroWrapper>
+    );
+}
--- src/templates/pitchPage.tsx
+++ src/templates/pitchPage.tsx
@@ -1,33 +1,26 @@
 import { graphql } from "gatsby";
-import { renderRichText } from "gatsby-source-contentful/rich-text";
 import React from "react";
 import styled, { ThemeProvider } from "styled-components";
 
 import GlobalStyle from "../global/globalStyle";
-import { portfolioOptions } from "../global/richTextOptions";
 import { renderPlainText } from "../global/textHelpers";
 import { LightTheme } from "../global/themes";
 
-import ButtonRight from "../components/Button";
 import { GalleryCarousel } from "../components/Carousel";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import { Layout, LayoutNarrow } from "../components/Layout";
-import MarkupSwap from "../components/MobileSwap";
 import Navbar from "../components/Navbar";
-import { Heading1, Heading2, Paragraph } from "../components/Typography";
 import { BlogCard } from "../components/cards/BlogCard";
 import { PortfolioCard } from "../components/cards/PortfolioCard";
-import Slideshow from "../components/media/Slideshow";
-import YouTube from "../components/media/YouTube";
 import FunFact from "../components/pitch/FunFact";
-import MiniService from "../components/pitch/MiniServiceCard";
+import Hero from "../components/pitch/Hero";
+import MiniServiceCard from "../components/pitch/MiniServiceCard";
 
-import { packRichText } from "../types/RichText";
 import Service, { PitchElement, getPitch } from "../types/Service";
 import MediaCollection from "../types/collections/MediaCollection";
 
-const Columns = styled.div`
+const PitchColumns = styled.div`
     display: flex;
     gap: 1rem;
 
@@ -43,49 +36,6 @@ const Columns = styled.div`
 const PitchElementWrapper = styled(LayoutNarrow)`
     margin: 2rem auto;
 `;
-
-const HeroHeading = styled(Heading1)`
-    font-size: 6vw;
-
-    @media (max-width: 1200px) {
-        font-size: 4rem;
-    }
-`;
-
-const HeroDetails = styled.div`
-    flex: 1;
-
-    ${Paragraph} {
-        max-width: 70%;
-    }
-`;
-
-const HeroSlideshow = styled.div`
-    flex: 1;
-`;
-
-const HeroButton = styled(ButtonRight)`
-    font-size: 2.5rem;
-
-    ${Heading2} {
-        padding: 0.1em;
-    }
-`;
-
-const Hero = styled.div`
-    display: flex;
-    gap: 1rem;
-
-    @media (max-width: 850px) {
-        flex-direction: column-reverse;
-    }
-`;
-
-type PitchPageProps = {
-    data: {
-        contentfulService: Service;
-    };
-};
 
 function renderPitchElement(
     element: PitchElement,
@@ -115,35 +65,35 @@ function renderPitchElement(
         case "ContentfulMiniServiceCollection":
             return (
                 <PitchElementWrapper key={key}>
-                    <Columns>
+                    <PitchColumns>
                         {element.items.map((item, idx) => (
-                            <MiniService
+                            <MiniServiceCard
                                 key={idx}
                                 src={item}
                                 to={`/cta/${item.slug}`}
                             />
                         ))}
-                    </Columns>
+                    </PitchColumns>
                 </PitchElementWrapper>
             );
         case "ContentfulPortfolioItemCollection":
             return (
                 <PitchElementWrapper key={key}>
-                    <Columns>
+                    <PitchColumns>
                         {element.items.map((item, idx) => (
                             <PortfolioCard item={item} key={idx} />
                         ))}
-                    </Columns>
+                    </PitchColumns>
                 </PitchElementWrapper>
             );
         case "ContentfulBlogPostCollection":
             return (
                 <PitchElementWrapper key={key}>
-                    <Columns>
+                    <PitchColumns>
                         {element.items.map((item, idx) => (
                             <BlogCard item={item} key={idx} />
                         ))}
-                    </Columns>
+                    </PitchColumns>
                 </PitchElementWrapper>
             );
         default:
@@ -154,6 +104,12 @@ function renderPitchElement(
             );
     }
 }
+
+type PitchPageProps = {
+    data: {
+        contentfulService: Service;
+    };
+};
 
 export default function ServicePage({ data }: PitchPageProps) {
     const pageData = data.contentfulService;
@@ -166,57 +122,13 @@ export default function ServicePage({ data }: PitchPageProps) {
             <Navbar active="services" />
 
             <Layout>
-                <Hero>
-                    <HeroDetails>
-                        <HeroHeading>{pageData.pitchTitle}</HeroHeading>
-
-                        <Paragraph>
-                            {renderRichText(
-                                packRichText(pageData.description),
-                                portfolioOptions,
-                            )}
-                        </Paragraph>
-
-                        <HeroButton to="#" type="normal" direction="right">
-                            {pageData.callToAction}
-                        </HeroButton>
-                    </HeroDetails>
-
-                    <HeroSlideshow>
-                        {!pageData.youTube && (
-                            <Slideshow
-                                src={pageData.slideshow.items}
-                                aspectRatio={16 / 9}
-                            />
-                        )}
-
-                        {pageData.youTube && (
-                            <YouTube
-                                url={pageData.youTube}
-                                aspectRatio={16 / 9}
-                            />
-                        )}
-                    </HeroSlideshow>
-                </Hero>
+                <Hero service={pageData} />
             </Layout>
 
             {pitchData.map((element, idx) =>
                 renderPitchElement(element, idx, pageData.buttonImages),
             )}
 
-            {/* 
-            <FunFact
-                author={pageData.teamMember}
-                fact="Did you know, 70% of businesses report an increase in sales after updating their Instagram and social media platforms?"
-                carousel={pageData.slideshow.items}
-                cta="Learn More"
-            />
-
-            <GalleryCarousel
-                images={pageData.slideshow.items}
-                targetHeight={1}
-            />
- */}
             <Footer />
         </ThemeProvider>
     );
