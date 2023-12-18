2a6b8c98abd5899d1200992e79958e8ee58da091
William Gardner <william@madebycounter.com>
2023-12-18 12:00:06 -0800

    Modal animations

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,23 +1,56 @@
 import React, { MouseEvent } from "react";
-import styled, { ThemeProvider } from "styled-components";
+import styled, { ThemeProvider, css } from "styled-components";
 
 import { LightTheme } from "../global/themes";
 
-type ModalWrapperProps = { $open: boolean };
+const ModalBackground = styled.div`
+    width: 100%;
+    height: 100%;
 
-const ModalWrapper = styled.div<ModalWrapperProps>`
-    display: ${(props) => (props.$open ? "flex" : "none")};
-    position: fixed;
-    width: calc(100% - 2rem);
-    height: calc(100% - 2rem);
-    background-color: #000000aa;
-    top: 0;
-    left: 0;
-    z-index: 9999;
-    padding: 1rem;
-
+    display: flex;
     justify-content: center;
     align-items: center;
+
+    background-color: #000000aa;
+
+    > * {
+        transform: scale(0.95);
+        transition: transform 200ms ease-out;
+    }
+`;
+
+const ModalWrapper = styled.div<{ $open: boolean }>`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 9999;
+
+    ${(props) =>
+        props.$open
+            ? css`
+                  transition:
+                      visibility 200ms ease-in-out,
+                      opacity 100ms ease-in-out;
+              `
+            : css`
+                  transition:
+                      visibility 200ms ease-in-out 200ms,
+                      opacity 100ms ease-in-out;
+              `}
+
+    visibility: hidden;
+    opacity: 0;
+
+    &.open {
+        visibility: visible;
+        opacity: 1;
+
+        ${ModalBackground} > * {
+            transform: scale(1);
+        }
+    }
 `;
 
 type ModalProps = {
@@ -29,13 +62,14 @@ type ModalProps = {
 export default function Modal({ children, open, setOpen }: ModalProps) {
     return (
         <ThemeProvider theme={LightTheme}>
-            <ModalWrapper
-                $open={open}
-                onClick={(e: MouseEvent) => {
-                    if (e.target === e.currentTarget) setOpen(false);
-                }}
-            >
-                {children}
+            <ModalWrapper $open={open} className={open ? "open" : ""}>
+                <ModalBackground
+                    onClick={(e: MouseEvent) => {
+                        if (e.target === e.currentTarget) setOpen(false);
+                    }}
+                >
+                    {children}
+                </ModalBackground>
             </ModalWrapper>
         </ThemeProvider>
     );
