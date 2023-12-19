20b0eef9cde4ba8894ba2fe1b8b680b551c5dc61
William Gardner <william@madebycounter.com>
2023-12-19 13:22:02 -0800

    Fixed duplicate reference bug

--- src/templates/portfolioItem.tsx
+++ src/templates/portfolioItem.tsx
@@ -320,7 +320,7 @@ const PortfolioItemPage = ({ data }: PortfolioItemProps) => {
 
                 <MobileLayout>
                     <div>
-                        <HeroMedia ref={mediaRef}>
+                        <HeroMedia>
                             {!youTube && (
                                 <Slideshow
                                     src={slideshow}
